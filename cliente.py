d86740b428def8c2cef7a56cf2d14a84d3ab1519
Beatriz Tavares Raposo <115116628+biaRaposo@users.noreply.github.com>
2023-03-06 17:38:55 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,18 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+cliente.connect(('localhost', 88))
+
+terminado = False
+print('Digite "tt" para terminar a mensagem: ')
+
+while not terminado:
+    cliente.send(input('Mensagem: ').encode('utf-8'))
+    msg = cliente.recv(1024).decode('utf-8')
+    if msg == 'tt':
+        terminado = True
+    else:
+        print(msg)
+
+cliente.close()
